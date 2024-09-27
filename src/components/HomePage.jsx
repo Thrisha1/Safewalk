7a540df9277fd1848ed1364dbd0342c60f65f67f
Thrisha1 <thrishakannan25@gmail.com>
2024-09-28 01:26:32 +0530

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,40 @@
+import React,{ useState, useEffect } from 'react'
+import SplashScreen from './SplashScreen.jsx'
+import OnBoardingScreen from "./OnBoardingScreen.jsx";
+import Loader from "./Loader.jsx";
+
+function HomePage() {
+    const [showSplash, setShowSplash] = useState(true);
+    const [screenSwitch, setScreenSwitch] = useState(0);
+
+    const handleScreenSwitch = (a) => {
+        setScreenSwitch(screenSwitch + a);
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setShowSplash(false);
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, []);
+  
+  return (
+    <div className="h-full flex items-center justify-center">
+        { showSplash ? (
+           <div className="">
+             <SplashScreen />
+             <p className="mt-4 text-white">Loading...</p>
+             <Loader />
+           </div>
+        ) : (
+            <div className="w-full h-full">
+                <OnBoardingScreen handleScreenSwitch={handleScreenSwitch} title="Way to Go" description=" dummy data description " button_name="Next" backgroundImage="/onboard-1.png" />
+                <OnBoardingScreen handleScreenSwitch={handleScreenSwitch} title="Way to Go" description=" dummy data description " button_name="Next" backgroundImage="/onboard-2.png" />
+                <OnBoardingScreen handleScreenSwitch={handleScreenSwitch} title="Way to Go" description=" dummy data description " button_name="Next" backgroundImage="/onboard-3.png" />
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+function SplashScreen() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+        <Image src='/logo.png' alt="splash" width="200" height="200" />
+    </div>
+  )
+}
+
+export default SplashScreen
